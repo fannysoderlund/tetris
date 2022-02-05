f63d4c6c942fa9f43cfc1183882f8ed9cc082475
Fanny <fannysarasoderlund@gmail.com>
2022-02-05 21:36:19 +0100

    Color scheme selection and save. Game mode and randomized tetrominos.

--- tetris.js
+++ tetris.js
@@ -2,6 +2,10 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const playButton = document.getElementById('gameStatus');
+const scoreLabel = document.getElementById('scoreLabel');
+const scoreCounter = document.getElementById('scoreCounter');
+const colorPicker = document.getElementById('colorPicker');
+const dropDownColor = document.getElementById('dropdown-content');
 
 context.scale(18, 18)
 
@@ -15,54 +19,63 @@ const tetrominos = [
   ],
 
   [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 1]
+    [0, 2, 0],
+    [0, 2, 0],
+    [0, 2, 2]
 
   ],
 
   [
-    [0, 1, 0],
-    [0, 1, 0],
-    [1, 1, 0]
+    [0, 3, 0],
+    [0, 3, 0],
+    [3, 3, 0]
 
   ],
 
   [
-    [1, 1],
-    [1, 1]
+    [4, 4],
+    [4, 4]
 
   ],
 
   [
-    [1, 1, 0],
-    [0, 1, 1],
+    [5, 5, 0],
+    [0, 5, 5],
     [0, 0, 0]
 
   ],
 
   [
-    [0, 1, 1],
-    [1, 1, 0],
+    [0, 6, 6],
+    [6, 6, 0],
     [0, 0, 0]
 
   ],
 
   [
     [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0]
+    [7, 7, 7],
+    [0, 7, 0]
 
   ],
 
 
 ];
 
+let currentColorScheme = 'standard';
+
+const colorsStandard = ['#1C1C1C', '#43AA8B', '#4D908E', '#90BE6D', '#F3722C', '#F8961E', '#F9844A', '#F9C74F'];
+const colorsPastel = ['#1C1C1C', '#BEE1E6', '#DFE7FD', '#E2ECE9', '#E3FDDF', '#FDE2E4', '#FDF8DF', '#FFF1E6'];
+const colorsForest = ['#1C1C1C', '#65442F', '#656C52', '#848382', '#9DA385', '#C3A580', '#C8875D', '#E2D5BE'];
+const colorsOldSchool = ['#1C1C1C', '#00E4FF', '#06FF30', '#9C1FFF', '#EBFF00', '#FA00FF', '#FF4A02', '#FFB800'];
+
+let score = 0;
+
 const gameBoard = generateGameboard(12, 22);
 
 const currentTetromino = {
   offset: { x: 5, y: -1 },
-  tetromino: tetrominos[5]
+  tetromino: tetrominos[Math.floor(Math.random() * 7)]
 }
 
 function collide(gameBoard, currentTetromino) {
@@ -100,7 +113,19 @@ function drawTetromino(tetrominoNumber, offset) {
   tetrominoNumber.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = '#43AA8B';
+        switch (currentColorScheme) {
+          case 'forest':
+            context.fillStyle = colorsForest[value];
+            break;
+          case 'pastel':
+            context.fillStyle = colorsPastel[value];
+            break;
+          case 'oldSchool':
+            context.fillStyle = colorsOldSchool[value];
+            break;
+          default:
+            context.fillStyle = colorsStandard[value];
+        }
         context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
@@ -120,10 +145,10 @@ document.addEventListener('keydown', event => {
 function dropTetromino() {
   currentTetromino.offset.y++;
   if (collide(gameBoard, currentTetromino)) {
-    console.log('collide')
     currentTetromino.offset.y--;
     populateGameBoard(gameBoard, currentTetromino);
     currentTetromino.offset.y = -1;
+    currentTetromino.tetromino = tetrominos[Math.floor(Math.random() * 7)]
   }
 
   dropCounter = 0;
@@ -133,6 +158,10 @@ let lastTime = 0;
 let dropCounter = 0;
 
 function play(time = 0) {
+  if (localStorage.getItem("colorScheme") != null) {
+    currentColorScheme = localStorage.getItem("colorScheme");
+  }
+  console.log(currentColorScheme)
   context.fillStyle = '#1C1C1C';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -149,8 +178,19 @@ function play(time = 0) {
   requestAnimationFrame(play);
 }
 
+function pickColorScheme(colorScheme) {
+  currentColorScheme = colorScheme;
+  localStorage.setItem("colorScheme", colorScheme);
+}
 
 playButton.addEventListener('click', () => {
+  playButton.style.color = '#1C1C1C';
+  playButton.style.pointerEvents = 'none';
+  playButton.style.cursor = 'none';
+  scoreLabel.innerHTML = 'SCORE';
+  colorPicker.style.color = '#1C1C1C';
+  colorPicker.style.pointerEvents = 'none';
+  dropDownColor.style.display = 'none';
   play();
 });
 
